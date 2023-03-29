23077e71bc5aa1120185c0e314fbe38e130825d2
Hubsy-kr <endbshal0709@naver.com>
2023-03-29 23:02:21 +0900

    complete navigaition bar UI

--- lib/features/main_navigation/main_navigation_screen.dart
+++ lib/features/main_navigation/main_navigation_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/features/main_navigation/widgets/nav_tab.dart';
 import 'package:tiktok_clone/features/main_navigation/widgets/post_video_button.dart';
+import 'package:tiktok_clone/features/video/video.timeline.screen.dart';
 
 import '../../constants/gaps.dart';
 import '../../constants/sizes.dart';
@@ -37,13 +38,8 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
       body: Stack(
         children: [
           Offstage(
-            offstage: _selectedIndex != 0,
-            child: const Center(
-              child: Text(
-                'Home',
-              ),
-            ),
-          ),
+              offstage: _selectedIndex != 0,
+              child: const VideoTimelineScreen()),
           Offstage(
             offstage: _selectedIndex != 1,
             child: const Center(
